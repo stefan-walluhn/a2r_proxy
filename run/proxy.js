c5d526de8e1cd5e3a22fa4d9632b53636fc92fbe
Stefan <stefan@terminal21.de>
2012-09-01 04:45:03 +0200

    client to backend data passthrue

--- run/proxy.js
+++ run/proxy.js
@@ -1,27 +1,38 @@
 var osc = require('osc-min') ;
 
 var argv = require('optimist').argv ;
-var udp = require('dgram').createSocket('udp4') ;
+var dgram = require('dgram') ;
+var udp_send = dgram.createSocket('udp4') ;
+var udp_res = dgram.createSocket('udp4') ;
 
 var proxy = {} ;
 var session = {} ;
 session['name'] = argv.oscname ;
 session['port'] = argv.oscport ;
-proxy.init = function() {
-}
+
+udp_res.on("message", function(msg, rinfo) {
+
+  var buf = osc.toBuffer({
+    address: argv.oscname ,
+    args: [{type: "integer", value: msg.readInt16LE(1)}]
+  }) ;
+
+  udp_send.send(buf, 0, buf.length, argv.oscport, argv.oschost) ;
+}) ;
+
 
 proxy.listen = function(port) {
-  console.log("New proxy listen on port: " + argv.proxyport) ;
+  console.log("New proxy listen on port: " + port) ;
+  udp_res.bind(port) ;
 }
 
-proxy.listen(argv.p) ;
-
+proxy.listen(argv.proxyport) ;
+/*
 function sendStuff() {
   var buf = osc.toBuffer({
     address: argv.oscname ,
     args: [{type: "integer", value: Math.floor(Math.random() * 127)}]
   }) ;
 
-  udp.send(buf, 0, buf.length, argv.oscport, argv.oschost) ;
-}
-setInterval(sendStuff, 2000) ;
+
+setInterval(sendStuff, 2000) ;*/
